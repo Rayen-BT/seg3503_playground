45d962a39ad17554ac224c8772f555f20a5ca9a1
Rayen Ben Tamarzizt <rayenbentamarzizt@gmail.com>
2026-06-27 16:17:44 -0400

    lab04 [green] group 4: testFizzbuzzReturnsFizzBuzz passes. added divisibility by 15 check at the top so multiples of both 3 and 5 are caught before the individual checks

--- lab04/fizzbuzz_java/src/main/java/fizzbuzz/FizzBuzz.java
+++ lab04/fizzbuzz_java/src/main/java/fizzbuzz/FizzBuzz.java
@@ -6,6 +6,8 @@ import java.util.List;
 public class FizzBuzz {
 
     public String fizzbuzz(int n) {
+        if (n % 15 == 0)
+            return "FizzBuzz";
         if (n % 3 == 0)
             return "Fizz";
         if (n % 5 == 0)
