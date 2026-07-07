d334ab0188a760019c4841a1b33e143ec0bce6aa
Rayen Ben Tamarzizt <rayenbentamarzizt@gmail.com>
2026-07-06 23:33:12 -0400

    twitter: fix isMentionned - handle null tweet and avoid substring false-match on @meat

--- lab05/twitter/src/Twitter.java
+++ lab05/twitter/src/Twitter.java
@@ -1,12 +1,14 @@
+import java.util.regex.Pattern;
+
 public class Twitter {
 
-  public String loadTweet()
-  {
+  public String loadTweet() {
     try {
       Thread.sleep(4000);
-    } catch (InterruptedException e) {}
+    } catch (InterruptedException e) {
+    }
 
-    double r =  Math.random();
+    double r = Math.random();
     if (r <= 0.45) {
       return "I am tweet that likes to talk about @me";
     } else if (r <= 0.9) {
@@ -18,7 +20,10 @@ public class Twitter {
 
   public boolean isMentionned(String name) {
     String tweet = loadTweet();
-    return tweet.contains("@" + name);
+    if (tweet == null) {
+      return false;
+    }
+    // Use a word boundary so "@meat" does not falsely match isMentionned("me")
+    return Pattern.compile("@" + Pattern.quote(name) + "\\b").matcher(tweet).find();
   }
-
 }
